bdfc774d35c71b6dfdae13dd0706e428477f594f
Max <massimiliano.baki@besquare.it>
2018-09-22 08:43:04 +0200

    Initial release

---
+++ PassportScanner/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PassportScanner
+//
+//  Created by Edwin Vermeer on 9/8/15.
+//  Copyright (c) 2015 Mirabeau. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
+
